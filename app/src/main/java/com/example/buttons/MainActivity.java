93f3150de5294ff1321261646004bd32183816bc
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-28 20:47:24 -0400

    Se agrega funcionalidad a botones y se modifica activity main para que se presente en Landscape. Pendiente crear Landscape.

--- app/src/main/java/com/example/buttons/MainActivity.java
+++ app/src/main/java/com/example/buttons/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.buttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.buttons.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initListeners();
+        }
+
+    private void initListeners() {
+        binding.buttonPlane.setOnClickListener(v -> Toast.makeText(this, "Modo avión desactivado", Toast.LENGTH_LONG).show());
+        binding.buttonUpdatingProfile.setOnClickListener(v -> Toast.makeText(this, "Perfil actualizado", Toast.LENGTH_LONG).show());
+        binding.buttonPictureSaved.setOnClickListener(v -> Toast.makeText(this, "Imagen guardada en galeria", Toast.LENGTH_LONG).show());
+        binding.buttonUnlocked.setOnClickListener(v -> Toast.makeText(this, "Nivel 5 desbloqueado", Toast.LENGTH_LONG).show());
+        binding.buttonLevel.setOnClickListener(v -> Toast.makeText(this, "Nivel 4 completado", Toast.LENGTH_LONG).show());
+        binding.buttonMusic.setOnClickListener(v -> Toast.makeText(this, "Musica pausada", Toast.LENGTH_LONG).show());
     }
 }
